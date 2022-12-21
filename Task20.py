797892d7d974ddd795c826e088864df2d6e15ce2
Uri_Klevcov <a-08-137.mail.ru>
2022-12-22 00:31:40 +0600

    Task20

--- Task20.py
+++ Task20.py
@@ -19,8 +19,54 @@
 # Ф, Щ, Ъ – 10 очков.
 # ------------------------------------
 # Напишите программу, которая вычисляет стоимость введенного пользователем слова.
-# Будем считать, что на вход подается только одно слово, которое содержит либо только английские, 
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
 # либо только русские буквы.
 # Ввод: ноутбук
 # Вывод: 12
 
+englishLetters = \
+    {
+        1: ['a', 'e', 'i', 'o', 'u', 'l', 'n', 's', 't', 'r'],
+        2: ['d', 'g'],
+        3: ['b', 'c', 'm', 'p'],
+        4: ['f', 'h', 'v', 'w', 'y'],
+        5: ['k',],
+        8: ['j', 'x'],
+        10: ['q', 'z']
+    }
+
+rusianLetters = \
+    {
+        1: ['а', 'в', 'е', 'и', 'н', 'о', 'р', 'с', 'е'],
+        2: ['д', 'к', 'л', 'м', 'п', 'у'],
+        3: ['б', 'г', 'ё', 'ь', 'я'],
+        4: ['й', 'ы'],
+        5: ['ж', 'з', 'х', 'ц', 'ч'],
+        8: ['ш', 'э', 'ю'],
+        10: ['ф', 'щ', 'ъ']
+    }
+
+userWord = input('Введите слово > ').lower()
+
+def Score(word, dict):
+    sum = 0
+    for letter in word:
+        for key, value in dict.items():
+            if letter in value:
+                sum += key
+                break
+    return sum
+
+def Dictionary(word):
+    english = set('qwertyuiopasdfghjklzxcvbnm')
+    beforeLength = len(english)
+    english.add(word[0])
+    afterLength = len(english)
+    if beforeLength == afterLength:
+        return True
+    return False
+
+if Dictionary(userWord):
+    print(f'Вы получили {Score(userWord, englishLetters)} очков')
+else:
+    print(f'Вы получили {Score(userWord, rusianLetters)} очков')
